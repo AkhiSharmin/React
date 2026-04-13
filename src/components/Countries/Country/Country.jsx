9ca58c58e1008c95a4d2a819347311eefa5ad9a7
AkhiSharmin <akhisharmin100@gmail.com>
2026-04-13 21:17:55 +0600

    some update

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Country.css";
 
 const Country = ({ country }) => {
   // console.log(country.population.population);
+  const [visited, setVisited] = useState(false);
 
   const handelVisited = () => {
-    console.log("Button clicked");
+    // option one
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+
+    // option two
+    // setVisited(visited ? false : true);
+
+    // option three
+    setVisited(!visited);
   };
 
   return (
-    <div className="country">
+    // <div className={`country ${visited ? "country-visited" : "Not visited"}`}>
+
+    <div className={`country ${visited && "country-visited"}`}>
       <img src={country.flags.flags.png} alt="" />
       <h3>Name: {country.name.common}</h3>
       <p>Population: {country.population.population}</p>
@@ -17,7 +31,9 @@ const Country = ({ country }) => {
         Area: {country.area.area}{" "}
         {country.area.area > 300000 ? "Big Country" : "Small Country"}
       </p>
-      <button onClick={handelVisited}>Not Visited</button>
+      <button onClick={handelVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
